a04e67eed9aac00cdedf7165ff62f310205307ee
SergiyLapiuk <72142067+SergiyLapiuk@users.noreply.github.com>
2022-05-31 21:47:46 +0300

    lab3

---
+++ laba3/simpleIteration/simpleIteration/simpleIteration.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void simpleIteration() {
+    double x = 1.25;
+    double y = 0;
+
+    double epsilon = 0.01;
+
+    double x_last = 0;
+    double y_last = 0;
+
+     do  {
+        
+        x_last = x;
+        y_last = y;
+
+        x = (0.9 + cos(y)) / 3;
+        y = sin(x - 0.6) - 1.6;
+     } while (max(abs(x - x_last), abs(y - y_last)) > epsilon);
+
+     cout << "Solution:" << endl;
+     cout << "x = " << x << "; y = " << y;
+} 
+
+int main()
+{
+    simpleIteration();
+    return 0;
+}
+
+
